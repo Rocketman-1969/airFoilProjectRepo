2c1306e63b7536a0e5429c8639061b597d31a676
Zachary Foster <zacharyfoster@Zacharys-MacBook-Pro-2.local>
2024-08-26 21:48:42 -0600

    made everything a class structure

--- AirFoilProjectMain.py
+++ AirFoilProjectMain.py
@@ -5,26 +5,86 @@ import matplotlib.pyplot as plt
 import json
 import GeometeryClass
 
+class Main:
+	"""
+	A class to represent the main application logic for the airfoil project.
 
+	Attributes
+	----------
+	config_file : str
+		The path to the input json  file.
+	radius : float
+		The radius of the cylinder.
+	x_low_val : float
+		The lower limit for the x-axis in the plot.
+	x_up_val : float
+		The upper limit for the x-axis in the plot.
+	geometry : GeometeryClass.Geometery
+		An instance of the Geometery class.
 
+	Methods
+	-------
+	load_config():
+		Loads the configuration from the JSON file.
+	setup_geometry():
+		Initializes the Geometery object and calculates the camber, upper surface, and lower surface.
+	plot():
+		Plots the camber line, upper surface, and lower surface.
+	run():
+		Executes the main logic of the application.
+	"""
 
-json_string = open('input.json').read()
-json_vals = json.loads(json_string)
-radius = json_vals['geometry']['cylinder_radius']
-x_low_val = json_vals['plot']['x_lower_limit']
-x_up_val = json_vals['plot']['x_upper_limit']
+	def __init__(self, config_file):
+		"""
+		Constructs all the necessary attributes for the Main object.
 
+		Parameters
+		----------
+		config_file : str
+			The path to the configuration file.
+		"""
+		self.config_file = config_file
+		
 
-geometry = GeometeryClass.Geometery(radius)
-camber, uppersurface, lower_surface = geometry.circle()
+	def load_config(self):
+		"""
+		Loads the configuration from the JSON file.
+		"""
+		with open(self.config_file, 'r') as file:
+			json_vals = json.load(file)
+		self.radius = json_vals['geometry']['cylinder_radius']
+		self.x_low_val = json_vals['plot']['x_lower_limit']
+		self.x_up_val = json_vals['plot']['x_upper_limit']
 
+	def setup_geometry(self):
+		"""
+		Initializes the Geometery object and calculates the camber, upper surface, and lower surface.
+		"""
+		self.geometry = GeometeryClass.Geometery(self.radius)
+		self.camber, self.upper_surface, self.lower_surface = self.geometry.circle()
 
-plt.figure()
-plt.plot(camber[0], camber[1], label='Camber Line')
-plt.plot(uppersurface[0], uppersurface[1], color='blue', label='Upper Surface')
-plt.plot(lower_surface[0], lower_surface[1], color='red', label='Lower Surface')
-plt.xlim(x_low_val, x_up_val)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.legend(loc='upper right')
-plt.show()
+	def plot(self):
+		"""
+		Plots the camber line, upper surface, and lower surface.
+		"""
+		plt.figure()
+		plt.plot(self.camber[0], self.camber[1], label='Camber Line')
+		plt.plot(self.upper_surface[0], self.upper_surface[1], color='blue', label='Upper Surface')
+		plt.plot(self.lower_surface[0], self.lower_surface[1], color='red', label='Lower Surface')
+		plt.xlim(self.x_low_val, self.x_up_val)
+		plt.gca().set_aspect('equal', adjustable='box')
+		plt.legend(loc='upper right')
+		plt.show()
+
+	def run(self):
+		"""
+		Executes the main logic of the application.
+		"""
+		self.load_config()
+		self.setup_geometry()
+		self.plot()
+
+if __name__ == "__main__":
+	main = Main('input.json')
+	main.run()
 
--- GeometeryClass.py
+++ GeometeryClass.py
@@ -2,14 +2,43 @@ import numpy as np
 
 
 class Geometery:
+    """
+    A class to represent the geometry of a circle.
+
+    Attributes
+    ----------
+    radius : float
+        The radius of the circle.
+
+    Methods
+    -------
+    circle():
+        Calculates the camber line, upper surface, and lower surface of the circle.
+    """
+
     def __init__(self, radius):
+        """
+        Constructs all the necessary attributes for the Geometery object.
+
+        Parameters
+        ----------
+        radius : float
+            The radius of the circle.
+        """
         self.radius = radius
 
     def circle(self):
-        # Define the geometery of a circle
+        """
+        Calculates the camber line, upper surface, and lower surface of the circle.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the camber line, upper surface, and lower surface as numpy arrays.
+        """
         angles = np.linspace(0, np.pi, 100)
-        upper_surface = np.array([self.radius*np.cos(angles), self.radius*np.sin(angles)])
-        lower_surface = np.array([self.radius*np.cos(angles), -self.radius*np.sin(angles)])
-        camber = np.array([[self.radius,-self.radius], [0,0]])
+        upper_surface = np.array([self.radius * np.cos(angles), self.radius * np.sin(angles)])
+        lower_surface = np.array([self.radius * np.cos(angles), -self.radius * np.sin(angles)])
+        camber = np.array([[self.radius, -self.radius], [0, 0]])
 
         return camber, upper_surface, lower_surface
