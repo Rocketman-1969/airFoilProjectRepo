a6cd6680c639125def106ef08e5d8ee8a011960e
Zachary Foster <zacharyfoster@Zacharys-MacBook-Pro-3.local>
2024-08-28 21:14:48 -0600

    Completed A5

--- AirFoilProjectMain.py
+++ AirFoilProjectMain.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import GeometeryClass
+import Flow
 
 
 class Main:
@@ -29,6 +30,10 @@ class Main:
 		Initializes the Geometery object and calculates the camber, upper surface, and lower surface.
 	plot():
 		Plots the camber line, upper surface, and lower surface.
+	surface_tangent(x):
+		Calculate the surface tangent vectors at a given x-coordinate.
+	surface_normal(x):
+		Calculate the surface normal vectors at a given x-coordinate.
 	run():
 		Executes the main logic of the application.
 	"""
@@ -61,92 +66,130 @@ class Main:
 		"""
 		self.geometry = GeometeryClass.Geometery(self.radius)
 		
+	def load_flow_field(self, V_inf, alpha):
+		"""
+		Loads the flow field parameters.
 
-	def surface_tangent(self, x):
-		
-		tangent_upper = np.array([x, np.sqrt(self.radius**2 - x**2)])
-		tangent_lower = np.array([x, -np.sqrt(self.radius**2 - x**2)])
+		Parameters:
+		V_inf (float): The free stream velocity.
+		alpha (float): The angle of attack.
+		"""
+		self.flow = Flow.Flow(self.radius, V_inf, alpha)
 
-		unit_tangent_upper = tangent_upper / np.linalg.norm(tangent_upper)
-		unit_tangent_lower = tangent_lower / np.linalg.norm(tangent_lower)
-
-		return unit_tangent_upper, unit_tangent_lower
-	
 	def surface_normal(self, x):
-		
-		unit_tangent_upper, unit_tangent_lower = self.surface_tangent(x)
+		"""
+		Calculate the surface normal vectors at a given x-coordinate.
+
+		Parameters:
+		x (float): The x-coordinate at which to calculate the surface normals.
+
+		Returns:
+		tuple: A tuple containing the unit normal vectors for the upper and lower surfaces.
+		"""
+		normal_upper = np.array([x, np.sqrt(self.radius**2 - x**2)])
+		normal_lower = np.array([x, np.sqrt(self.radius**2 - x**2)])
+
+		unit_normal_upper = normal_upper / np.linalg.norm(normal_upper)
+		unit_normal_lower = normal_lower / np.linalg.norm(normal_lower)
+
+		return unit_normal_upper, unit_normal_lower
+	
+	def surface_tangent(self, x):
+		"""
+		Calculate the surface tangent vectors at a given x-coordinate.
+
+		Parameters:
+		x (float): The x-coordinate at which to calculate the surface tangents.
+
+		Returns:
+		tuple: A tuple containing the unit tangent vectors for the upper and lower surfaces.
+		"""
+		unit_tangent_upper, unit_tangent_lower = self.surface_normal(x)
 
 		z = np.array([0, 0, 1])
 
 		tangent_upper = np.cross(unit_tangent_upper, z)
-		tangent_lower = np.cross(unit_tangent_lower, z)
+		tangent_lower = np.cross(-unit_tangent_lower, z)
 
 		unit_tangent_upper = tangent_upper / np.linalg.norm(tangent_upper)
 		unit_tangent_lower = tangent_lower / np.linalg.norm(tangent_lower)
 
+		unit_tangent_lower = unit_tangent_lower[:2]
+		unit_tangent_upper = unit_tangent_upper[:2]
+
 		return unit_tangent_upper, unit_tangent_lower
+	
+	def surface_tangential_velocity(self, x):
+		"""
+		Calculate the tangential velocity at a given x-coordinate.
 
-		
-	def plot(self, camber, upper_surface, lower_surface):
-		plt.figure()
-		plt.plot(camber[0], camber[1], label='Camber Line')
-		plt.plot(upper_surface[0], upper_surface[1], color='blue', label='Upper Surface')
-		plt.plot(lower_surface[0], lower_surface[1], color='red', label='Lower Surface')
-		plt.xlim(self.x_low_val, self.x_up_val)
-		plt.gca().set_aspect('equal', adjustable='box')
-		plt.legend(loc='upper right')
-		plt.xlabel('X')
-		plt.ylabel('Y')
-		plt.title('Airfoil Geometry')
-		plt.show()
+		Parameters:
+		x (float): The x-coordinate at which to calculate the tangential velocity.
 
+		Returns:
+		tuple: A tuple containing the tangential velocity for the upper and lower surfaces.
+		"""
+		unit_tangent_upper, unit_tangent_lower = self.surface_tangent(x)
+
+		velocity_upper = np.dot(self.flow.flow_over_cylinder_cartesin(x, np.sqrt(self.radius**2 - x**2)), unit_tangent_upper)
+		velocity_lower = np.dot(self.flow.flow_over_cylinder_cartesin(x, -np.sqrt(self.radius**2 - x**2)), unit_tangent_lower)
+
+		return velocity_upper, velocity_lower
+
+	
 	def run(self):
 		"""
 		Executes the main logic of the application.
 		"""
 		self.load_config()
 		self.setup_Geometry()
+		self.load_flow_field(1, 0)
 
-		# set up X array
-		x = np.linspace(-1 * self.radius, self.radius, 1000)
-		camber = np.empty((2, 0))
-		upper_surface = np.empty((2, 0))
-		lower_surface = np.empty((2, 0))
+		Velocity = self.flow.flow_over_cylinder_cartesin(4, 0)
+		print(Velocity)
 
-		for i in range(len(x)):
-			camber_temp, upper_surface_temp, lower_surface_temp = self.geometry.circle(x[i])
+		velocity_upper, velocity_lower = self.surface_tangential_velocity(1)
+		print("Velocity Upper: ", velocity_upper)
+		print("Velocity Lower: ", velocity_lower)
 
-			# Append the new values to the arrays
-			camber = np.hstack((camber, camber_temp.reshape(2, 1)))
-			upper_surface = np.hstack((upper_surface, upper_surface_temp.reshape(2, 1)))
-			lower_surface = np.hstack((lower_surface, lower_surface_temp.reshape(2, 1)))
+
+
+
+		# # set up X array
+		# x = np.linspace(-1 * self.radius, self.radius, 1000)
+		# camber = np.empty((2, 0))
+		# upper_surface = np.empty((2, 0))
+		# lower_surface = np.empty((2, 0))
+
+		# for i in range(len(x)):
+		# 	camber_temp, upper_surface_temp, lower_surface_temp = self.geometry.circle(x[i])
+
+		# 	# Append the new values to the arrays
+		# 	camber = np.hstack((camber, camber_temp.reshape(2, 1)))
+		# 	upper_surface = np.hstack((upper_surface, upper_surface_temp.reshape(2, 1)))
+		# 	lower_surface = np.hstack((lower_surface, lower_surface_temp.reshape(2, 1)))
+
+		# unit_normal_upper, unit_normal_lower = self.surface_normal(-2)
+		# unit_tangent_upper, unit_tangent_lower = self.surface_tangent(-2)
+
+		# # Print the results
+		# print("normal_upper: ", unit_normal_upper)
+		# print("normal_lower: ", unit_normal_lower)
+		# print("tangent_upper: ", unit_tangent_upper)
+		# print("tangent_lower: ", unit_tangent_lower)
 
 		# Plotting the results
-		plt.figure()
-		plt.plot(camber[0, :], camber[1, :], label='Camber')
-		plt.plot(upper_surface[0, :], upper_surface[1, :], label='Upper Surface')
-		plt.plot(lower_surface[0, :], lower_surface[1, :], label='Lower Surface')
-		plt.legend(loc='upper right')
-		plt.xlabel('X')
-		plt.ylabel('Y')
-		plt.title('Airfoil Geometry')
-		plt.show()
-
-		
-		
+		# plt.figure()
+		# plt.plot(camber[0, :], camber[1, :], label='Camber')
+		# plt.plot(upper_surface[0, :], upper_surface[1, :], label='Upper Surface')
+		# plt.plot(lower_surface[0, :], lower_surface[1, :], label='Lower Surface')
+		# plt.legend(loc='upper right')
+		# plt.xlabel('X')
+		# plt.ylabel('Y')
+		# plt.title('Airfoil Geometry')
+		# plt.show()
 
 if __name__ == "__main__":
 	main = Main('input.json')
 	main.run()
 		
-
-
-
-		
-		
-		
-
-if __name__ == "__main__":
-	main = Main('input.json')
-	main.run()
-
--- Flow.py
+++ Flow.py
@@ -1,2 +1,25 @@
 import numpy as np
 
+class Flow:
+
+    def __init__(self, radius, V_inf, alpha):
+        self.radius = radius
+        self.V_inf = V_inf
+        self.alpha = alpha
+
+    def flow_over_cylinder_cartesin(self, x, y):
+        """
+        Calculates the camber line, upper surface, and lower surface of the circle.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the camber line, upper surface, and lower surface as numpy arrays.
+        """
+
+        f_1 = self.V_inf * (1 - (self.radius**2 / (x**2 + y**2)))*((x / np.sqrt(x**2 + y**2))*np.cos(self.alpha) + (y / np.sqrt(x**2 + y**2))*np.sin(self.alpha))
+        f_2 = self.V_inf * (1 + (self.radius**2 / (x**2 + y**2)))*((y / np.sqrt(x**2 + y**2))*np.cos(self.alpha) - (y / np.sqrt(x**2 + y**2))*np.cos(self.alpha))
+
+        velocity = np.array([f_1*(x/(np.sqrt(x**2 + y**2))) - f_2*(y/np.sqrt(x**2 + y**2)), f_1*(y/(np.sqrt(x**2 + y**2))) + f_2*(x/np.sqrt(x**2 + y**2))])
+
+        return velocity
